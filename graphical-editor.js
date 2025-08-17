23c46580cd7dfb63ce046ffb66280b29a2f83f67
Matthew-Sher <gomaruz75@gmail.com>
2025-08-17 21:17:36 +0300

    Added choice of color and refactored conditions of interactivity of a shape

--- graphical-editor.js
+++ graphical-editor.js
@@ -4,6 +4,19 @@ for (let btn of colorPicker.getElementsByTagName('button')) {
     btn.style.setProperty('background-color', btn.dataset.color);
 };
 
+let color = '#000000'
+colorPicker.addEventListener('click', function (event) {
+    let target = event.target;
+    let btn = target.closest('button.color');
+    if (btn) {
+        for (button of event.currentTarget.getElementsByTagName('button')) {
+            button.classList.remove('selected');
+        };
+        btn.classList.add('selected');
+        color = btn.dataset.color;
+    };
+})
+
 let canvas = document.getElementById('canvas');
 let startCoord = {};
 
@@ -15,16 +28,16 @@ canvas.addEventListener('mousedown', function (event) {
 
 let currentShape;
 canvas.addEventListener('mousemove', function (event) {
-    let endCoord = {
-        x: event.pageX,
-        y: event.pageY,
-    };
-
-    if (event.button == 0 && event.buttons == 1) {
+    if (event.buttons == 0) {
+        return;
+    } else {
+        let endCoord = {
+            x: event.pageX,
+            y: event.pageY,
+        };
         if (currentShape !== null && currentShape !== undefined) {
             canvas.removeChild(currentShape);
-        }
-
+        };
         currentShape = drawLine(startCoord, endCoord);
     };
 })
@@ -43,7 +56,7 @@ let drawLine = function (startCoords, endCoords) {
     line.setAttribute('y1', startCoords.y);
     line.setAttribute('x2', endCoords.x);
     line.setAttribute('y2', endCoords.y);
-    line.setAttribute('stroke', '#000000')
+    line.setAttribute('stroke', color);
     canvas.appendChild(line);
     return line;
 };
