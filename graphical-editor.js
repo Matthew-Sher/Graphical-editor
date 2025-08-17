7586491532090337e40e36e3a1c27af4826cf6ef
Matthew-Sher <gomaruz75@gmail.com>
2025-08-17 19:38:11 +0300

    Added tool "Line"

--- graphical-editor.js
+++ graphical-editor.js
@@ -4,4 +4,30 @@ for (let btn of colorPicker.getElementsByTagName('button')) {
     btn.style.setProperty('background-color', btn.dataset.color);
 };
 
+let canvas = document.getElementById('canvas');
+let startCoord = {};
 
+canvas.addEventListener('mousedown', function (event) {
+    startCoord.x = event.pageX;
+    startCoord.y = event.pageY;
+    console.log(event);
+    console.log(`(${event.pageX}; ${event.pageY})`);
+});
+
+canvas.addEventListener('mouseup', function (event) {
+    let endCoord = {
+        x: event.pageX,
+        y: event.pageY,
+    };
+    drawLine(startCoord, endCoord);
+});
+
+let drawLine = function (startCoords, endCoords) {
+    let line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+    line.setAttribute('x1', startCoords.x);
+    line.setAttribute('y1', startCoords.y);
+    line.setAttribute('x2', endCoords.x);
+    line.setAttribute('y2', endCoords.y);
+    line.setAttribute('stroke', '#000000')
+    canvas.appendChild(line);
+};
