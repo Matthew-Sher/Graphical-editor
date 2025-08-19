37faefbd7b81b3e9821bee7a5395e50632a8ba36
Matthew-Sher <gomaruz75@gmail.com>
2025-08-19 17:06:18 +0300

    Added square (rectangle with pressed shift)

--- graphical-editor.js
+++ graphical-editor.js
@@ -9,9 +9,7 @@ colorPicker.addEventListener('click', function (event) {
     let target = event.target;
     let btn = target.closest('button.color');
     if (btn) {
-        for (button of event.currentTarget.getElementsByTagName('button')) {
-            button.classList.remove('selected');
-        };
+        event.currentTarget.querySelector('button.selected').classList.remove('selected');
         btn.classList.add('selected');
         color = btn.dataset.color;
     };
@@ -23,9 +21,7 @@ shapePicker.addEventListener('click', function (event) {
     let target = event.target;
     let btn = target.closest('button.shape');
     if (btn) {
-        for (button of event.currentTarget.getElementsByTagName('button')) {
-            button.classList.remove('selected');
-        };
+        event.currentTarget.querySelector('button.selected').classList.remove('selected');
         btn.classList.add('selected');
         shape = btn.dataset.shape;
     };
@@ -48,10 +44,15 @@ canvas.addEventListener('mousemove', function (event) {
             x: event.pageX,
             y: event.pageY,
         };
+        let equal = false;
+        if (event.shiftKey) {
+            equal = true;
+        };
         if (currentShape !== null && currentShape !== undefined) {
             canvas.removeChild(currentShape);
         };
-        currentShape = drawShape(shape, startCoord, endCoord);
+
+        currentShape = drawShape(shape, startCoord, endCoord, equal);
     };
 })
 
@@ -60,16 +61,22 @@ canvas.addEventListener('mouseup', function (event) {
         x: event.pageX,
         y: event.pageY,
     };
-    drawShape(shape, startCoord, endCoord);
+
+    let equal = false;
+    if (event.shiftKey) {
+        equal = true;
+    };
+
+    drawShape(shape, startCoord, endCoord, equal);
 });
 
-let drawShape = function (shape, startCoord, endCoord) {
+let drawShape = function (shape, startCoord, endCoord, equal) {
     if (shape == 'line') {
-        return drawLine(startCoord, endCoord)
+        return drawLine(startCoord, endCoord);
     } else if (shape == 'circle') {
         return drawCircle(startCoord, endCoord);
     } else if (shape == 'rectangle') {
-        return drawRectangle(startCoord, endCoord);
+        return drawRectangle(startCoord, endCoord, equal);
     } else {
         console.log('Error')
         return undefined;
@@ -88,8 +95,8 @@ let drawLine = function (startCoords, endCoords) {
 };
 
 let calcRadius = function (startCoords, endCoords) {
-    let a = Math.abs(startCoord.x - endCoords.x);
-    let b = Math.abs(startCoord.y - endCoords.y);
+    let a = Math.abs(startCoords.x - endCoords.x);
+    let b = Math.abs(startCoords.y - endCoords.y);
     let r = Math.sqrt(a ** 2 + b ** 2);
     return r;
 };
@@ -105,12 +112,23 @@ let drawCircle = function (startCoords, endCoords) {
     return Circle;
 };
 
-let drawRectangle = function (startCoords, endCoords) {
+let drawRectangle = function (startCoords, endCoords, equal) {
     let Rectangle = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
-    Rectangle.setAttribute('x', Math.min(startCoords.x, endCoords.x));
-    Rectangle.setAttribute('y', Math.min(startCoords.y, endCoords.y));
-    Rectangle.setAttribute('width', Math.abs(endCoords.x - startCoords.x));
-    Rectangle.setAttribute('height', Math.abs(endCoords.y - startCoord.y));
+    if (equal) {
+        Rectangle.setAttribute('y', Math.min(startCoords.y, endCoords.y));
+        if (startCoords.x > endCoords.x) {
+            Rectangle.setAttribute('x', startCoords.x - Math.abs(endCoords.y - startCoords.y));
+        } else {
+            Rectangle.setAttribute('x', startCoords.x);
+        };
+        Rectangle.setAttribute('width', Math.abs(endCoords.y - startCoords.y));
+        Rectangle.setAttribute('height', Math.abs(endCoords.y - startCoord.y));
+    } else {
+        Rectangle.setAttribute('x', Math.min(startCoords.x, endCoords.x));
+        Rectangle.setAttribute('y', Math.min(startCoords.y, endCoords.y));
+        Rectangle.setAttribute('width', Math.abs(endCoords.x - startCoords.x));
+        Rectangle.setAttribute('height', Math.abs(endCoords.y - startCoord.y));
+    };
     Rectangle.setAttribute('stroke', color);
     Rectangle.setAttribute('fill', 'none');
     canvas.appendChild(Rectangle);
